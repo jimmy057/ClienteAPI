8548374db7a7652b94768991bac53d5f06b7e039
jimmy057 <jimmypolanco2540@gmail.com>
2025-10-15 01:32:16 -0400

    Add project files.

---
+++ Modelo/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace ClienteAPI.Modelo
+{
+	public class Cliente
+	{
+		public int Id { get; set; }             
+		public string Nombre { get; set; } = "";
+		public string Apellido { get; set; } = "";
+		public string Email { get; set; } = "";
+		public string Telefono { get; set; } = "";
+		public string Direccion { get; set; } = "";
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using ClienteAPI.DAL;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var conStr = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
